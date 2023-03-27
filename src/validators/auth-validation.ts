5f3c77602382c4ec735d02b48df8c9b47401f6c7
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2023-03-27 13:22:04 +0300

    corrected auth

--- src/validators/auth-validation.ts
+++ src/validators/auth-validation.ts
@@ -4,6 +4,7 @@ import {userService} from "../services/user-service";
 import bcrypt from "bcrypt";
 import {HTTP_STATUSES} from "../http_statuses";
 import {blogService} from "../services/blog-service";
+import {userRepository} from "../repositories/user-repositpry";
 
 const emailPattern = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
 export const checkIsValidUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -45,7 +46,7 @@ const loginValidation = body('login')
     .isLength({min: 1, max: 25}).withMessage('Not correct length')
     .notEmpty().withMessage('Field must not be empty')
     .custom(async value => {
-        const isUserExists = await userService.getUserByLoginOrEmail(value)
+        const isUserExists = await userRepository.getUserByLoginOrEmail({ "accountData.userName": value})
         if(!isUserExists) {
             throw new Error();
         }
@@ -69,7 +70,7 @@ const emailValidation = body('email')
     .custom(value => emailPattern.test(value)).withMessage('Is not email!')
     .notEmpty().withMessage('Field must not be empty')
     .custom(async value => {
-        const isUserExists = await userService.getUserByLoginOrEmail(value)
+        const isUserExists = await userRepository.getUserByLoginOrEmail({ "accountData.email": value})
         if(!isUserExists) {
             throw new Error();
         }
